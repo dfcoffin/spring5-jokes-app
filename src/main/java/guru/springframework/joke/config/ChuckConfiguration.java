318a7ad1f711e17ed248e1d5d916d8e466d8740e
Donald F Coffin <donald.coffin@reminetworks.com>
2017-09-12 16:46:41 -0400

    Add Java Configuration support

--- src/main/java/guru/springframework/joke/config/ChuckConfiguration.java
+++ src/main/java/guru/springframework/joke/config/ChuckConfiguration.java
@@ -1,7 +1,17 @@
 package guru.springframework.joke.config;
 
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * Created by Donald F. Coffin on 9/12/17
  */
+@Configuration
 public class ChuckConfiguration {
+
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes() {
+        return new ChuckNorrisQuotes();
+    }
 }
