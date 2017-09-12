e8855e45cae6787cd94b576da052a5c7e198b209
Donald F Coffin <donald.coffin@reminetworks.com>
2017-09-12 17:06:07 -0400

    Add XML Configuration support

--- src/main/java/guru/springframework/joke/config/ChuckConfiguration.java
+++ src/main/java/guru/springframework/joke/config/ChuckConfiguration.java
@@ -1,16 +1,14 @@
 package guru.springframework.joke.config;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by Donald F. Coffin on 9/12/17
  */
-@Configuration
+//@Configuration
 public class ChuckConfiguration {
 
-    @Bean
+//    @Bean
     public ChuckNorrisQuotes chuckNorrisQuotes() {
         return new ChuckNorrisQuotes();
     }
