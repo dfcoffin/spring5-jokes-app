b6ecc729c44776ae2a13edb1bb7917e9d9d7b301
Donald F Coffin <donald.coffin@reminetworks.com>
2017-09-12 12:39:47 -0400

    Add JokeService

---
+++ src/main/java/guru/springframework/joke/services/JokeService.java
@@ -0,0 +1,9 @@
+package guru.springframework.joke.services;
+
+/**
+ * Created by Donald F. Coffin on 9/12/17
+ */
+public interface JokeService {
+
+    String getJoke();
+}
---
+++ src/main/java/guru/springframework/joke/services/JokeServiceImpl.java
@@ -0,0 +1,22 @@
+package guru.springframework.joke.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Donald F. Coffin on 9/12/17
+ */
+@Service
+public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
